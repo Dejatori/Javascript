93cec4b863e283f40f95339314ee8cfd8fe3bff2
D3jatori01~ <deivitsc@gmail.com>
2023-02-13 19:57:52 -0500

    Para que copien

--- ejercicios/metodos.js
+++ ejercicios/metodos.js
@@ -2,3 +2,6 @@ let texto = "Hola Mundo!"; // Definimos una variable de tipo string
 console.log(texto.charAt(3)); // Devuelve el carácter de la posición 3
 console.log(texto.charCodeAt(0)); // Devuelve el código ASCII de la posición 0
 console.log(texto.concat(" ", "Adios Mundo!")); // Concatena dos cadenas
+console.log(texto.endsWith("!")); // Devuelve true si la cadena termina con el carácter especificado
+console.log(texto.indexOf("Mundo")); // Devuelve la posición de la primera aparición de la cadena especificada
+console.log(texto.lastIndexOf("!")); // Devuelve la posición de la última aparición de la cadena especificada
