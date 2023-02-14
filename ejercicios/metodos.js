4e8ee19cc39c7f8ec135537c10cb8c54db489fdb
D3jatori01~ <deivitsc@gmail.com>
2023-02-13 19:50:42 -0500

    new file:   ejercicios/metodos.js

---
+++ ejercicios/metodos.js
@@ -0,0 +1,4 @@
+let texto = "Hola Mundo!"; // Definimos una variable de tipo string
+console.log(texto.charAt(3)); // Devuelve el carácter de la posición 3
+console.log(texto.charCodeAt(0)); // Devuelve el código ASCII de la posición 0
+console.log(texto.concat(" ", "Adios Mundo!")); // Concatena dos cadenas
