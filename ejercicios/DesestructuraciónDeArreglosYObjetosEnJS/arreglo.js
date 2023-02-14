841ee16a16957a3756ec9e25eb45c0d4f8819d57
D3jatori01~ <deivitsc@gmail.com>
2023-02-14 18:34:09 -0500

    Desestructuración de Arreglos

---
+++ ejercicios/DesestructuraciónDeArreglosYObjetosEnJS/arreglo.js
@@ -0,0 +1,9 @@
+// ¿Por qué es esto necesario?
+// Imagina que queremos extraer datos de un arreglo. Anteriormente, ¿cómo se haría esto?
+
+let introduccion = ["Hola", "Yo", "soy", "Sarah", "Connor"]; // Arreglo de 5 elementos
+let saludo = introduccion[0]; 
+let nombre = introduccion[3];
+
+console.log(saludo); // "Hola"
+console.log(nombre); // "Sarah"
---
+++ ejercicios/DesestructuraciónDeArreglosYObjetosEnJS/arregloDeJS.js
@@ -0,0 +1,5 @@
+let introduccion = ["Hola", "Yo", "soy", "Sarah", "Connor"]; // Arreglo de 5 elementos
+let [saludo, pronombre] = introduccion; // Desestructuración de arreglos
+
+console.log(saludo); // "Hola"
+console.log(pronombre); // "Yo"
