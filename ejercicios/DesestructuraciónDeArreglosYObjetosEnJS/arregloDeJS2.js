fc0bfcdf4ed93b0d754ff83de014d750db5adff9
D3jatori01~ <deivitsc@gmail.com>
2023-02-14 19:02:02 -0500

    arreglo terminator

--- ejercicios/DesestructuraciónDeArreglosYObjetosEnJS/arregloDeJS2.js
+++ ejercicios/DesestructuraciónDeArreglosYObjetosEnJS/arregloDeJS2.js
@@ -2,3 +2,19 @@ let [saludo, pronombre] = ["Hola", "Yo", "soy", "Sarah", "Connor"]; // Desestruc
 
 console.log(saludo); // "Hola"
 console.log(pronombre); // "Yo"
+
+let [Terminator] = ["Hasta la vista, baby", "I will back", "Dame esa chaqueta"]; 
+
+console.log(Terminator); // "Hasta la vista, baby"
+
+let [John, , Hijo] = ["Lidera la resistencia", "Es el futuro", "Es el hijo de Sarah Connor"]; 
+
+console.log(John, Hijo); // "Lidera la resistencia" "Es el hijo de Sarah Connor"
+
+let [T800, , Killer] = ["Es un cyborg", "Es un Terminator", "Es un asesino"];
+
+console.log(T800, Killer); // "Es un cyborg" "Es un asesino"
+
+let [Skynet, red, criminal] = ["Es una IA", "Es una red de computadoras", "Es una organización criminal"];
+
+console.log(Skynet, criminal); // "Es una IA" "Es una organización criminal"
